0ce40d124126d534c382ce496dcec061ca7e3415
Anas Lamaiz <lamaizanass@gmail.com>
2023-05-28 20:39:28 +0100

    fixed react error

--- client/src/components/rating/Rating.js
+++ client/src/components/rating/Rating.js
@@ -3,10 +3,19 @@ import './Rating.css';
 const Stars = ({ value }) => {
     const starsElement = []
     for (let i = 0; i < Math.floor(value); i++) {
-        starsElement.push(<i className="fa fa-star" aria-hidden="true"></i>)
+        starsElement.push(<i
+            key={i}
+            className="fa fa-star"
+            aria-hidden="true"></i>)
     }
     for (let i = 0; i < 5 - Math.floor(value); i++) {
-        starsElement.push(<i className="fa fa-star disabled" aria-hidden="true"></i>)
+        starsElement.push(
+            <i
+                key={i + Math.floor(value)}
+                className="fa fa-star disabled"
+                aria-hidden="true">
+            </i>
+        )
     }
     return starsElement;
 }
