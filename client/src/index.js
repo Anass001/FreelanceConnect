585a290e3093bee48f8268ec599a68515e2fd2aa
Anas Lamaiz <lamaizanass@gmail.com>
2023-05-28 20:35:50 +0100

    apollo subscriptions & registered service worker

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,35 @@ import './index.css';
 import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { split } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'ws://localhost:4000/graphql',
+}));
 
 const httpLink = createUploadLink({
   uri: 'http://localhost:4000/graphql',
   credentials: 'include'
 });
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
@@ -21,3 +42,8 @@ root.render(
     <App />
   </ApolloProvider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
