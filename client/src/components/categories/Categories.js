20f4dfe9eb1c4b92783469e5fdc3ba50275e444c
Anas Lamaiz <lamaizanass@gmail.com>
2023-05-28 20:37:44 +0100

    now getting categories dynamically

--- client/src/components/categories/Categories.js
+++ client/src/components/categories/Categories.js
@@ -2,8 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Categories.css";
 
-const Categories = () => {    
-
+function Categories({ categories }) {
     return (
         <div className="categories">
             <ul>
@@ -15,38 +14,21 @@ const Categories = () => {
                         <span>Home</span>
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to="/categories/design" className={({ isActive }) => {
-                        if (isActive) return "active-link";
-                        return "inactive-link";
-                    }}>
-                        <span>Design</span>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/categories/web" className={({ isActive }) => {
-                        if (isActive) return "active-link";
-                        return "inactive-link";
-                    }}>
-                        <span>Web</span>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/categories/mobile" className={({ isActive }) => {
-                        if (isActive) return "active-link";
-                        return "inactive-link";
-                    }}>
-                        <span>Mobile</span>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/categories/photography" className={({ isActive }) => {
-                        if (isActive) return "active-link";
-                        return "inactive-link";
-                    }}>
-                        <span>Photography</span>
-                    </NavLink>
-                </li>
+                {
+                    categories.map((category, index) => {
+                        return (
+                            <li key={index}>
+                                <NavLink to={`/categories/${category.url_name}`} className={({ isActive }) => {
+                                    if (isActive) return "active-link";
+                                    return "inactive-link";
+                                }}>
+                                    <span>{category.name}</span>
+                                </NavLink>
+                            </li>
+                        );
+                    }
+                    )
+                }
             </ul>
         </div>
     );
