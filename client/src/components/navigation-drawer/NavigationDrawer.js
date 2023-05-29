9a16a95ff1f50b0a2cc45dd79d2f223bee534117
Anas Lamaiz <lamaizanass@gmail.com>
2023-05-29 19:36:25 +0100

    made navigation bar responsive

---
+++ client/src/components/navigation-drawer/NavigationDrawer.js
@@ -0,0 +1,13 @@
+
+function NavigationDrawer() {
+    // navigation drawer without using material-ui
+    return (
+        <div className="navigation-drawer__wrapper">
+            <div className="navigation-drawer__icon">
+                <span className="material-symbols-outlined">menu</span>
+            </div>
+        </div>
+    )
+}
+
+export default NavigationDrawer;
