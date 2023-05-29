245e020d36cf6e9e5525031fa7e626d6d7304512
Anas Lamaiz <lamaizanass@gmail.com>
2023-05-29 19:37:10 +0100

    categories are dynamic now

--- client/src/pages/Category/Category.js
+++ client/src/pages/Category/Category.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Categories from '../../components/categories/Categories';
 import ServiceCard from '../../components/service-card/ServiceCard';
 import { gql, useQuery } from '@apollo/client';
 import LoadingIndicator from '../../components/loading-indicator/LoadingIndicator';
+import { useParams } from 'react-router-dom';
 import '../Home/Home.css';
-import { NavLink, useParams } from 'react-router-dom';
 
-const GET_SERVICES_BY_CATEGORY = gql`
-    query GetServicesByCategory($category: String!) {
-        servicesByCategory(category: $category) {
+const GET_CATEGORIES = gql`
+    query GetCategories {
+        categories {
             _id
+            name
+            url_name
+        }
+    }
+`;
+
+const GET_SERVICES_BY_CATEGORY_URL_NAME = gql`
+    query GetServicesByCategory($categoryUrlName: String!) {
+        servicesByCategoryUrlName(categoryUrlName: $categoryUrlName) {
+            _id
+            title
             description
             price
             images
@@ -26,26 +37,35 @@ function Services() {
 
     const { category } = useParams();
 
-    const { loading, error, data } = useQuery(GET_SERVICES_BY_CATEGORY, {
-        variables: { category: category },
+    const { loading, error, data } = useQuery(GET_SERVICES_BY_CATEGORY_URL_NAME, {
+        variables: {
+            categoryUrlName: category
+        },
     });
+
     if (loading) return <LoadingIndicator />
     if (error) return `Error! ${error.message}`;
     return (
         <div className="services__grid__wrapper row">
-            {data.servicesByCategory.map((service) => (
-                <div className="col-xs-12 col-sm-8 col-md-6 col-lg-3">
-                    <ServiceCard key={service._id} service={service} />
-                </div>
-            ))}
+            {data &&
+                data.servicesByCategoryUrlName.map((service) => (
+                    <div className="col-xs-12 col-sm-8 col-md-6 col-lg-3">
+                        <ServiceCard key={service._id} service={service} />
+                    </div>
+                ))}
         </div>
     );
 }
 
 function Category() {
+
+    const { data } = useQuery(GET_CATEGORIES);
+
     return (
         <div>
-            <Categories />
+            {
+                data && <Categories categories={data.categories} />
+            }
             <Services />
         </div>
     );
